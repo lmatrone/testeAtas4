3f1f6a669de7d280cf8158e7a11c4c65ea1b40c6
Leonardo Matrone <50384620+lmatrone@users.noreply.github.com>
2019-06-22 21:09:43 -0300

    Create abi.js

---
+++ abi.js
@@ -0,0 +1,175 @@
+const   contractABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "paramPauta",
+				"type": "string"
+			},
+			{
+				"name": "paramDecisao",
+				"type": "string"
+			},
+			{
+				"name": "paramData",
+				"type": "string"
+			},
+			{
+				"name": "paramParticipantes",
+				"type": "string"
+			}
+		],
+		"name": "ArquivarAta",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "time",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "book1",
+		"outputs": [
+			{
+				"name": "pauta",
+				"type": "string"
+			},
+			{
+				"name": "decisao",
+				"type": "string"
+			},
+			{
+				"name": "data",
+				"type": "string"
+			},
+			{
+				"name": "participantes",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "book2",
+		"outputs": [
+			{
+				"name": "pauta",
+				"type": "string"
+			},
+			{
+				"name": "decisao",
+				"type": "string"
+			},
+			{
+				"name": "data",
+				"type": "string"
+			},
+			{
+				"name": "participantes",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_data",
+				"type": "string"
+			}
+		],
+		"name": "buscaPorData",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "numeroAta",
+				"type": "uint256"
+			}
+		],
+		"name": "ConsultaDeliberacoesConselho",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "mostrarNumeroAtas",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+    
+    if (network === "4") {
+        contractAddress = "0x6ce2d9efbc23dda8f5e4f09fbc31e3af5e3c0cd4"; 
+    
+    } else {
+    contractAddress = "0x6ce2d9efbc23dda8f5e4f09fbc31e3af5e3c0cd4"; 
+}
+
+contract = web3.eth.contract(contractABI).at(contractAddress);
